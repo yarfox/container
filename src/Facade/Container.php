ac01bc0dd1b4a8720b259105fbc14159f1898b35
anhoder <anhoderai@xiaoman.cn>
2021-08-22 20:57:01 +0800

    feat: Facade

--- src/Facade/Container.php
+++ src/Facade/Container.php
@@ -8,11 +8,24 @@
 
 namespace Anhoder\Container\Facade;
 
+use Anhoder\Container\Constant\Constant;
 use Anhoder\Container\Contract\Facade;
 use Anhoder\Container\Container as RealContainer;
 
 /**
- * @mixin \Anhoder\Container\Container
+ * @method static registerProducer(string $key, mixed $producer, string $scope = Constant::SCOPE_PROTOTYPE)
+ * @method static registerSingletonProducer(string $key, mixed $producer)
+ * @method static mixed getProducer(string $key)
+ * @method static registerInstance(string $key, object $instance)
+ * @method static mixed getInstance(string $key)
+ * @method static mixed resolve(string $key)
+ * @method static mixed resolveClass(string $class)
+ * @method static registerConfigs(array $configs)
+ * @method static array getConfigs()
+ * @method static registerConfig(string $key, mixed $value)
+ * @method static mixed getConfig(string $key, mixed $default = null)
+ * @method static get(string $id)
+ * @method static bool has(string $id)
  * @see \Anhoder\Container\Container
  */
 class Container extends Facade
